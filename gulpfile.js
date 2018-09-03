f9924b46438575ad3b6afc660e78a97aa7d261c6
Neil <neilb2009@hotmail.co.uk>
2018-09-03 17:35:40 +0100

    Browser Sync and copy file added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync    = require('browser-sync');
+var reload         = browserSync.reload;
 
 var SOURCEPATH = {
     imgSource:  'src/img/',
     jsSource:   'src/js/',
-    scssSource: 'src/scss/*.scss'
+    scssSource: 'src/scss/*.scss',
+    htmlSource: 'src/*.html'
 }
 var APPPATH = {
     root:  'app/',
@@ -17,8 +20,22 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(APPPATH.css))
 });
 
-gulp.task('watch', ['sass'], function(){
+gulp.task('serve', ['sass'], function(){
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+        server: {
+            baseDir: APPPATH.root
+        }
+    })
+});
+
+gulp.task('copy', function(){
+    gulp.src(SOURCEPATH.htmlSource)
+    .pipe(gulp.dest(APPPATH.root))
+});
+
+gulp.task('watch', ['serve','sass','copy'], function(){
     gulp.watch([SOURCEPATH.scssSource], ['sass']);
+    gulp.watch([SOURCEPATH.htmlSource], ['copy']);
 });
 
 gulp.task('default', ['watch']);
