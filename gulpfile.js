d95b3f31ac751946b668047ea6c9eabdd1e1fa10
Neil <neilb2009@hotmail.co.uk>
2018-09-03 21:40:03 +0100

    Autoprefixer added

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp           = require('gulp');
+var sass           = require('gulp-sass');
 var browserSync    = require('browser-sync');
 var reload         = browserSync.reload;
 var clean          = require('gulp-clean');
+var autoprefixer   = require('gulp-autoprefixer');
 
 var SOURCEPATH = {
     imgSource:  'src/img/**',
     jsSource:   'src/js/**',
     scssSource: 'src/scss/*.scss',
-    htmlSource: 'src/*.html'
+    htmlSource: 'src/*.html',
+    imgSource:  'src/img/**'
 }
 var APPPATH = {
     root:  'app/',
     css:   'app/css',
-    js:   'app/js',
-    fonts:'app/fonts',
-    img:  'app/img'
+    js:    'app/js',
+    fonts: 'app/fonts',
+    img:   'app/img'
 }
 
 gulp.task('sass', function(){
     gulp.src(SOURCEPATH.scssSource)
+    .pipe(autoprefixer())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest(APPPATH.css))
 });
@@ -31,12 +34,7 @@ gulp.task('serve', ['sass'], function(){
         }
     })
 });
-/*
-gulp.task('copy', function(){
-    gulp.src(SOURCEPATH.htmlSource)
-    .pipe(gulp.dest(APPPATH.root))
-});
-*/
+
 gulp.task('html', function(){
     return gulp.src(SOURCEPATH.htmlSource)
     .pipe(gulp.dest(APPPATH.root))
@@ -47,16 +45,28 @@ gulp.task('clean-html', function(){
     .pipe(clean())
 });
 
+gulp.task('scripts', ['clean-scripts'], function(){
+    gulp.src(SOURCEPATH.jsSource)
+    .pipe(gulp.dest(APPPATH.js))
+});
+
 gulp.task('clean-scripts', function(){
     return gulp.src(APPPATH.js + '/*.js', {read: false, force: true})
     .pipe(clean())
 });
 
-gulp.task('watch', ['serve','sass','clean-html','html'], function(){
+gulp.task('images', function(){
+    return gulp.src(SOURCEPATH.imgSource)
+    .pipe(gulp.dest(APPPATH.img))
+});
+
+gulp.task('watch', ['serve','sass','clean-html','clean-scripts','scripts','images','html'], function(){
     gulp.watch([SOURCEPATH.scssSource], ['sass']);
-    // gulp.watch([SOURCEPATH.htmlSource], ['copy']);
+    gulp.watch([SOURCEPATH.jsSource],   ['scripts']);
     gulp.watch([SOURCEPATH.htmlSource], ['html']);
+    gulp.watch([SOURCEPATH.imgSource],  ['images']);
 });
 
 gulp.task('default', ['watch']);
 
+//['serve','sass','clean-html','clean-scripts','scripts','moveFonts','images','html']
